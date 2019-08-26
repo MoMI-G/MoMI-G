6aa8bce6653f64743ee0c7f7f7b3cfbd60dc9d69
Toshiyuki Yokoyama <6br@users.noreply.github.com>
2019-08-27 00:52:20 +0900

    Support sv callers (#10)
    * Add SURVIVOR support
    
    * Add 10X LongRanger support

--- scripts/vcf2gfa/pcf2gfa.sh
+++ scripts/vcf2gfa/pcf2gfa.sh
@@ -7,7 +7,7 @@ reference=$2
 pref=$input
 
 sort -t","  -k 7n $input > $pref.output.pcf
-awk -F "[,:]" '{print $1,"\t",$2,"\n",$4,"\t",$5}' $input | sed -e 's/[ ]*//g' | sort -k 1,1 -k 2,2n | uniq > $pref.bp.tsv
+awk -F "[,:]" '{print $1,"\t",$2,"\n",$4,"\t",$5}' <(sed '1d' $input) | sed -e 's/[ ]*//g' | sort -k 1,1 -k 2,2n | uniq > $pref.bp.tsv
 ruby vcf2gfa/json_to_breakpoint_list.rb $pref.bp.tsv $reference > breakpoint_list_tmp.tsv
 cat $pref.bp.tsv breakpoint_list_tmp.tsv | sort -k 1,1 -k 2,2n | uniq > $pref.bp.merged.tsv
 if [ ! -s $reference.fa.fai ]; then 
--- scripts/vcf2gfa/vcf2pcf.rb
+++ scripts/vcf2gfa/vcf2pcf.rb
@@ -3,18 +3,65 @@
 # For inter-chromosomal dataset.
 
 INTMAX=1000*1000*1000
+mode_10x=false
 puts "source_id,source_breakpoint,source_strand,target_id,target_breakpoint,target_strand,priority,svtype,gt,allele,id"
 
 STDIN.each do |line|
-  next if line.start_with?("#")
+  if line.start_with?("#")
+    mode_10x = true if line.start_with?("##source=10X")
+    next
+  end
   line = line.split("\t")
-  #next if line[2].end_with?("_2")  # For 10X dataset
+  next if line[2].end_with?("_2") if mode_10x # For 10X dataset
   info = line[7]
   info_hash = {}
   info.split(";").each{|t| a = t.split("=");info_hash[a[0]] = a[1]}
-  line[6] = info_hash["SVLEN"].to_i.abs
-  line[7] = line[4][1..-2]
+  if mode_10x
+    line[6] = info_hash["PAIRS"].to_i.abs 
+  elsif info_hash["SVMETHOD"].start_with?("Sniffles")
+    line[6] = info_hash["SVLEN"].to_i.abs
+  elsif info_hash["SVMETHOD"].start_with?("SURVIVOR")
+    line[6] = info_hash["AVGLEN"].to_i.abs
+  else
+    line[6] = info_hash["SVLEN"].to_i.abs # It depends on an implementation.
+  end
+  if mode_10x
+    line[7] = info_hash["SVTYPE2"] ? info_hash["SVTYPE2"] : info_hash["SVTYPE"]
+    line[7] = line[7] === "BND" ? "TRA" : line[7]
+  else
+    line[7] = line[4][1..-2]
+  end
 
+  if mode_10x
+    if match = line[4].match(/(.?)([\[\]])(chr.*)\:(\d*)([\[\]])(.?)/)
+      line[3] = match[3]
+      line[4] = match[4]
+      if match[1] != "" && match[2] == "]"
+        line[2] = "+"
+        line[5] = "-"
+      elsif match[1] == "" && match[2] == "["
+        line[2] = "-"
+        line[5] = "+"
+      elsif match[1] != "" && match[2] == "["
+        line[2] = "+"
+        line[5] = "+"
+      else
+        line[2] = "-"
+        line[5] = "-"
+      end
+      line[5] = line[5] == "-" ? "+" : "-" ###DANGER: It works ONLY 10X large_svs.vcf
+    else
+      line[3] = line[0]
+      line[4] = info_hash["END"]
+      #if line[7] == "DUP"
+      #  line[2] = "-"
+      #  line[5] = "+"
+      #else
+        line[2] = "+"
+        line[5] = "+"
+      #end
+    end
+  else
     line[3] = info_hash["CHR2"]
     line[4] = info_hash["END"]
     if info_hash["STRANDS"] == "++" 
@@ -30,10 +77,15 @@ STDIN.each do |line|
       line[2] = "+"
       line[5] = "+"
     end
+    if info_hash["SVMETHOD"].start_with?("SURVIVOR")
+      line[0] = "chr" + line[0]
+      line[3] = "chr" + info_hash["CHR2"]
+    end
+  end
   next if line[3] == "chrM" || line[0] == "chrM"
   path_name = line[0] + "_" + line[1].to_s + ".." + line[3] + "_" + line[4].to_s 
   line[9] = line[9].chomp
   line[10] = "#{line[7].downcase}_#{path_name}"
 
-  puts line.join(",")  if line[0]!=""
+  puts line[0..10].join(",")  if line[0] != ""
 end
