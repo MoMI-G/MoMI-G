0cdb80ec6512042d63eb23907744ce08e5428a7d
Toshiyuki Yokoyama <tfdvrt@gmail.com>
2020-02-05 13:49:12 +0900

    Update script

--- scripts/vcf2gfa/json_to_breakpoint_list.rb
+++ scripts/vcf2gfa/json_to_breakpoint_list.rb
@@ -1,8 +1,6 @@
 #!/bin/ruby
 #
 
-require "json"
-
 interval = 1*1000*1000
 chrs = {}
 hash = {}
@@ -16,12 +14,8 @@ File.foreach(ARGV[1] + ".fai") do |line|
   hash[line[0]] = line[1].to_i
 end
 
-#File.open(__dir__ + "/GRCh.json") do |file|
-#  hash = JSON.load(file)
-#
-  chrs.to_a.each do |chr_name, id| 
-    next if chr_name == "source_id" || chr_name == "target_id"
-    chr_len = hash[chr_name] #hash[ARGV[1]].find{|t| t["label"] == chr_name}
-    [*1..chr_len / interval].each{|t|  puts [chr_name, t * interval].join("\t") } if chr_len
-  end
-#end
+chrs.to_a.each do |chr_name, id| 
+  next if chr_name == "source_id" || chr_name == "target_id"
+  chr_len = hash[chr_name] #hash[ARGV[1]].find{|t| t["label"] == chr_name}
+  [*1..chr_len / interval].each{|t|  puts [chr_name, t * interval].join("\t") } if chr_len
+end
