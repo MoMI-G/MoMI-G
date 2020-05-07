eeabeba73e8663aeedc8de3226292a2c89817aca
Toshiyuki Yokoyama <tfdvrt@gmail.com>
2020-05-07 14:37:39 +0900

    Change reference name identification

--- scripts/vcf2gfa/json_to_breakpoint_list.rb
+++ scripts/vcf2gfa/json_to_breakpoint_list.rb
@@ -14,7 +14,7 @@ File.foreach(ARGV[1] + ".fai") do |line|
   hash[line[0]] = line[1].to_i
 end
 
-chrs.to_a.each do |chr_name, id| 
+hash.to_a.each do |chr_name, _id|
   next if chr_name == "source_id" || chr_name == "target_id"
   chr_len = hash[chr_name] #hash[ARGV[1]].find{|t| t["label"] == chr_name}
   [*1..chr_len / interval].each{|t|  puts [chr_name, t * interval].join("\t") } if chr_len
