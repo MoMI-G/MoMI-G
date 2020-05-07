be80401d3f87dcdd464583d6c845292541ba1c2b
Toshiyuki Yokoyama <tfdvrt@gmail.com>
2020-05-07 16:13:34 +0900

    Fix if condition

--- scripts/vcf2gfa/pcf2gfa.sh
+++ scripts/vcf2gfa/pcf2gfa.sh
@@ -10,7 +10,7 @@ sort -t"," -k 7n $input > $pref.output.pcf
 awk -F "[,:]" '{print $1,"\t",$2,"\n",$4,"\t",$5}' <(sed '1d' $input) | sed -e 's/[ ]*//g' | sort -k 1,1 -k 2,2n | uniq > $pref.bp.tsv
 if [ ! -s $reference.fai ]; then
   if [ ! -s $reference ]; then
-    if [! -s $reference.fa ]; then 
+    if [ ! -s $reference.fa ]; then 
       rsync -avPq --ignore-existing rsync://hgdownload.cse.ucsc.edu/goldenPath/$reference/bigZips/$reference.fa.gz .
       gunzip -c $reference.fa.gz > $reference.fa
     fi
